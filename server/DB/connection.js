84e83ebf015f2e1d809d294ad69984067be1952b
anvi <anvimile04042002@gmail.com>
2024-01-26 17:06:49 +0530

    first commit

---
+++ server/DB/connection.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+mongoose
+  .connect("mongodb://localhost:27017/edureify", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("connection start"))
+  .catch((error) => {
+    console.error(error.message);
+  });
---
+++ server/index.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+require("./DB/connection");
+const cors = require("cors");
+const router = require("./Routes/route")
+app.use(cors());
+app.use(express.json());
+app.use(router);
+app.listen(5000, () => {
+  console.log("server is running at port 5000");
+});
